bf621279a883d62c0d190d2dea264d55fb0b4279
Ilya Elichev <git.user.ilyaelichev@gmail.com>
2024-11-13 09:40:51 +0400

    Что-то изменил Что-то добавил

---
+++ src/main/java/com/stepup/Box.java
@@ -0,0 +1,8 @@
+package com.stepup;
+
+public class Box {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+
+}
--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,11 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("1111111");
+        System.out.println("1001");
+    }
+
+
+    public static void doSmth(String[] args) {
+        System.out.println(1);
     }
 }
