6da204ff0cd0f92a05c89bf3dc590761561b2077
Ilya Elichev <git.user.ilyaelichev@gmail.com>
2024-11-13 09:42:35 +0400

    Поменял 2 строки

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -9,5 +9,7 @@ public class MainApplication {
 
     public static void doSmth(String[] args) {
         System.out.println(1);
+        System.out.println("2");
+        System.out.println("3");
     }
 }
