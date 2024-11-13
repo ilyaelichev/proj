342a36b22cde2c0441ac1fd2631902d630733276
Ilya Elichev <git.user.ilyaelichev@gmail.com>
2024-11-13 09:30:04 +0400

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,6 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("111111");
+        System.out.println("1111111");
     }
 }
